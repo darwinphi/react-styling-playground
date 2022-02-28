a7f5ce56418847095943fac496e101e384afbd41
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-28 12:37:03 +0800

    Refactor app to use styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
 import { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  animation: App-logo-spin infinite 20s linear;
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppButton = styled.button`
+  font-size: calc(10px + 2vmin);
+`;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppWrapper>
+      <AppHeader>
+        <AppLogo src={logo} />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <AppButton onClick={() => setCount((count) => count + 1)}>
             count is: {count}
-          </button>
+          </AppButton>
         </p>
-      </header>
-    </div>
+      </AppHeader>
+    </AppWrapper>
   );
 }
 
